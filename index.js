856bf88fa03494d84ed17ca249ed4149f14f4bcb
codeoD <david_lee_w@163.com>
2019-01-25 14:04:20 +0800

    my apollo engine test

---
+++ index.js
@@ -0,0 +1,55 @@
+const { ApolloServer, gql } = require('apollo-server')
+const faker = require('faker')
+
+// console.log(faker.company.companyName(), faker.name.findName())
+const books = [
+  {
+    title: faker.company.companyName(),
+    author: faker.name.findName()
+  },
+  {
+    title: faker.company.companyName(),
+    author: faker.name.findName()
+  }
+]
+
+const typeDefs = gql`
+  type Book {
+    title: String,
+    author: String
+  }
+
+  type MockInfo {
+    a: String
+    b: String
+  }
+
+  type Query {
+    books: [Book],
+    test: MockInfo
+  }
+
+  type Mutation {
+    addBook (title: String, author: String) : Book
+  }
+`
+const resolvers = {
+  Query: {
+    books: () => books
+    // mock: () => test
+  },
+  Mutation: {
+    addBook: (a, book) => {
+      console.log(book)
+      return book
+    }
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  mocks: true
+})
+
+server.listen().then(({ url }) => console.log(`Server ready at ${url}`))
